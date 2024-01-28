04942d837e29542afcb94b46800c368124dde877
DanielKuris <danielkuris6@gmail.com>
2024-01-28 22:41:20 +0200

    Fixed Order

--- main.c
+++ main.c
@@ -6,26 +6,18 @@ int main()
     int start, end;
     scanf("%d %d", &start, &end);
 
-    printf("The Armstrong numbers are:");
-    for(int i = start; i <= end; i++)
-    {
-        if(isArmstrong(i))
-            printf(" %d", i);
-    }
-    printf("\n");
-
-    printf("The Palindromes are:");
-    for(int i = start; i <= end; i++)
-    {
-        if(isPalindrome(i))
-            printf(" %d", i);
-    }
-    printf("\n");
-
     printf("The Prime numbers are:");
     for(int i = start; i <= end; i++)
     {
         if(isPrime(i))
+            printf(" %d", i);
+    }
+    printf("\n");
+
+    printf("The Armstrong numbers are:");
+    for(int i = start; i <= end; i++)
+    {
+        if(isArmstrong(i))
             printf(" %d", i);
     }
     printf("\n");
@@ -39,5 +31,14 @@ int main()
     printf("\n");
 
     return 0;
+
+    printf("The Palindromes are:");
+    for(int i = start; i <= end; i++)
+    {
+        if(isPalindrome(i))
+            printf(" %d", i);
+    }
+    printf("\n");
+
     
 }
